2682ca8b61b7934b93ea0d2b65959d233e806521
wangguangwu <wangguangwu@pukanghealth.com>
2024-08-14 14:59:35 +0800

    first commit

---
+++ src/main/java/com/wangguangwu/springbootstandard/SpringBootStandardApplication.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.springbootstandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStandardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wangguangwu/springbootstandard/SpringBootStandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.springbootstandard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStandardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
